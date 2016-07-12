d66d8bc6764c24baa99e020e5c9c7162fcf52b5f
GIT <907064772@qq.com>
2016-07-12 16:06:28 +0800

    Initial Commit

---
+++ runtime各种测试/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  runtime各种测试
+//
+//  Created by luodp on 16/7/12.
+//  Copyright © 2016年 zhanghao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
